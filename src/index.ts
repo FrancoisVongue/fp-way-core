3813de7bd8abc642167249773761447f79dc48b8
Francois Vongue <francoisvongue@gmail.com>
2025-05-30 17:07:07 -0300

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
+// Core functionality
 export * from './core'
 export * from './core.types'
+
+// Type-specific namespaces
 export * from './bool'
 export * from './num'
 export * from './str'
 export * from './obj'
 export * from './arr'
 export * from './union'
+export * from './form'
+
+// Re-export namespaces for convenience
+export { bool } from './bool'
+export { num } from './num'
+export { str } from './str'
+export { obj } from './obj'
+export { arr } from './arr'
+export { Union } from './union'
+export { Form } from './form'
